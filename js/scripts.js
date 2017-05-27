feeae0a489493a0ae400b18866796d935a5afb20
grace <gtong8@my.bcit.ca>
2017-05-27 16:38:01 -0700

    Working Mobile Nav & Scrolling
    - Working sticky nav with dropdown
    - Logo hover (switched to background img link in css)
    - Added smooth scrolling in js
    - Added more css resets
    - Added accessibility features (tested using screen reader program
    chromevox)

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,7 @@
 const $body = $('body');
 const $btnMenu = $('.btnMenu');
 
+
 // show mobile nav
 $body.removeClass('show-mobile-nav');
 
@@ -12,3 +13,24 @@ $btnMenu.click(function(){
 });
 
 
+// hide nav menu after clicking a link
+$('#mainNav a').click(function() {
+	if($body.hasClass('show-mobile-nav')){
+		$body.removeClass('show-mobile-nav');
+	};
+});
+
+
+// smooth scroll 
+$('a[href^="#"]').on('click',function (e) {
+    e.preventDefault();
+
+    const target = this.hash;
+    const $target = $(target);
+
+    $('html, body').stop().animate({
+        'scrollTop': $target.offset().top - 70
+    }, 900, 'swing', function () {
+        window.location.hash = 1+ target;
+    });
+});
